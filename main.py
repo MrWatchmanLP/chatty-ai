c5dcbf751aa8b614a1c49f8607968e2014fc0afd
MrWatchmanLP <mrwatchmanlp@gmail.com>
2019-10-22 14:41:04 +0300

    Анекдот + генерация (по идее)

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import random
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+import mc
 
 token = "8ebd06e6b39901e266522e3bfb750bec56002534ff8d0f9faa8acc1b2a28422770ed7eca2e0dbe30e4ea5"
 vk_session = vk_api.VkApi(token=token)
 session_api = vk_session.get_api()
 longpoll = VkBotLongPoll(vk_session, 178923582)
+learning_data_local = []
+counter = 0
 
 
 def create_keyboard(response):
@@ -35,10 +38,22 @@ def send_message(session_api, peer_id, message=None, attachment=None, keyboard=N
 while True:
     for event in longpoll.listen():
         if event.type == VkBotEventType.MESSAGE_NEW:
+            if event.obj.text.lower()[:25] != "[club178923582|@chattyai]":
+                learning_data_local.append(event.obj.text.lower())
+                counter = counter + 1
+            if counter == 50:
+                counter = 0
+                generator = mc.StringGenerator(learning_data=learning_data_local, order=1)
+                send_message(session_api, event.obj.peer_id, message=generator.generate(count=1))
             if event.obj.peer_id != event.obj.from_id:
-                if event.obj.text.lower()[:25] == "[club178923582|@chattyai]" and event.obj.text.lower().find("команды") > -1:
-                    send_message(session_api, event.obj.peer_id, message="Команды бота: \n \n 1) @chattyai рыгни")
-                if event.obj.text.lower()[:25] == "[club178923582|@chattyai]" and event.obj.text.lower().find("рыгни") > -1:
+                if event.obj.text.lower()[:25] == "[club178923582|@chattyai]" and event.obj.text.lower().find(
+                        "команды") > -1:
+                    send_message(session_api, event.obj.peer_id, message="Команды бота: \n \n 1) @chattyai рыгни\n 2) @chattyai анекдот")
+                if event.obj.text.lower()[:25] == "[club178923582|@chattyai]" and event.obj.text.lower().find(
+                        "рыгни") > -1:
                     send_message(session_api, event.obj.peer_id, message="*Рыгает*")
+                if event.obj.text.lower()[:25] == "[club178923582|@chattyai]" and event.obj.text.lower().find(
+                        "анекдот") > -1:
+                    send_message(session_api, event.obj.peer_id, message="Колобок повесился")
             if event.obj.peer_id == event.obj.from_id:
                 send_message(session_api, event.obj.from_id, message="Каво?")
